ad7cc6e414ea6cc06a4dce379224f72a9ce98e71
AlexCavalcantiJordao <alex37705732@gmail.com>
2023-06-28 11:56:38 -0300

    build/ build.xml manifest.mf nbproject/ src/

---
+++ src/incrememnto/matematico/IncrememntoMatematico.java
@@ -0,0 +1,12 @@
+package incrememnto.matematico;
+
+public class IncrememntoMatematico {
+
+    /* Método Principal da classe */
+    public static void main(String[] args) {
+        int x = 8;
+        int y = ++x; // Pré-Requisito é quando o "++x" fica na frente do valor dar um item a mais no valor. Agora Pós-Requisito é quando o "x++" deixa o mesmo valor que é incrementado.
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+    }// Fim do método...
+}// Fim da classe...
